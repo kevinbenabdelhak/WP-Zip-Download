8c54af6674c8a68663d38c11cac1a0f5f7526459
Kevin Benabdelhak <163947458+kevinbenabdelhak@users.noreply.github.com>
2025-07-05 04:58:54 +0200

    Update wp-zip-download.php

--- wp-zip-download.php
+++ wp-zip-download.php
@@ -4,7 +4,7 @@
 Plugin Name: WP Zip Download
 Plugin URI: https://kevin-benabdelhak.fr/plugins/wp-zip-download/
 Description: Ajoutez une fonctionnalité à votre site WordPress permettant de télécharger des fichiers médias en tant qu'archive ZIP directement depuis la bibliothèque de médias. Configurez un intervalle de temps pour supprimer automatiquement les fichiers ZIP avec une option de réglage personnalisée accessible via le tableau de bord, permettant une gestion flexible et efficace de l'espace de stockage.
-Version: 1.0
+Version: 1.1
 Author: Kevin BENABDELHAK
 Author URI: https://kevin-benabdelhak.fr/
 Contributors: kevinbenabdelhak
